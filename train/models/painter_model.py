a080bfc4d90848e620de2acdd81b874b79a72604
Mikhail Lopatin <mixoz3101@gmail.com>
2023-04-10 15:46:35 +0200

    debug

--- train/models/painter_model.py
+++ train/models/painter_model.py
@@ -317,31 +317,29 @@ class PainterModel(BaseModel):
         print("param_1", param_1.shape)
         print("param_2", param_2.shape)
         s_1, c_1, e_1, mu_1, w_1 = torch.split(param_1, (2, 2, 2, 2, 1), dim=-1)
-        print("s_1", s_1.shape)
-        print("c_1", c_1.shape)
-        print("e_1", e_1.shape)
-        print("mu_1", mu_1.shape)
-        print("w_1", w_1.shape)
-
-
+        s_1 = s_1.squeeze(-1)
+        c_1 = c_1.squeeze(-1)
+        e_1 = e_1.squeeze(-1)
         h_1 = self.length_quadratic_bezier_curve(s_1, c_1, e_1)
         theta_1 = torch.atan2(e_1[:, 0] - s_1[:, 0], e_1[:, 1] - s_1[:, 1])
         print("theta_1", theta_1.shape)
 
         # mu_1, w_1, h_1, theta_1 = torch.split(param_1, (2, 1, 1, 1), dim=-1)
         w_1 = w_1.squeeze(-1)
-        h_1 = h_1.squeeze(-1)
+        # h_1 = h_1.squeeze(-1)
         theta_1 = torch.acos(torch.tensor(-1., device=param_1.device)) * theta_1.squeeze(-1)
         print("theta_1", theta_1.shape)
         trace_1 = (w_1 ** 2 + h_1 ** 2) / 4
 
         s_2, c_2, e_2, mu_2, w_2 = torch.split(param_2, (2, 2, 2, 2, 1), dim=-1)
-
+        s_2 = s_2.squeeze(-1)
+        c_2 = c_2.squeeze(-1)
+        e_2 = e_2.squeeze(-1)
         h_2 = self.length_quadratic_bezier_curve(s_2, c_2, e_2)
         theta_2 = torch.atan2(e_2[:, 0] - s_2[:, 0], e_2[:, 1] - s_2[:, 1])
         # mu_2, w_2, h_2, theta_2 = torch.split(param_2, (2, 1, 1, 1), dim=-1)
         w_2 = w_2.squeeze(-1)
-        h_2 = h_2.squeeze(-1)
+        # h_2 = h_2.squeeze(-1)
         theta_2 = torch.acos(torch.tensor(-1., device=param_2.device)) * theta_2.squeeze(-1)
         trace_2 = (w_2 ** 2 + h_2 ** 2) / 4
         print("gauss", w_1.shape, h_1.shape, theta_1.shape)
